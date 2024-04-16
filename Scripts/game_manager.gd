8598be1404c3b4f9955f10cacec30ed464c0f52f
Jordan <jordansjsalazar@gmail.com>
2024-04-15 19:35:39 -0700

    added level with custom tileset

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -8,10 +8,14 @@ func _ready():
 	root = get_tree().root
 	player = get_node("/root/Level/SubViewportContainer/SubViewport/Player")
 	ui = get_node("/root/Level/SubViewportContainer/CanvasLayer/UI")
+	debug()
 
 func player_death():
 	ui.show()
 	player.reset_pos()
+
+func debug():
+	get_node("/root/Level/SubViewportContainer/SubViewport/TileMap").queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
