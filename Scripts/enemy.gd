729cf112aed9eaa905cdc81f7b4b6369ea199b1c
Jordan <jordansjsalazar@gmail.com>
2024-04-19 04:48:03 -0700

    stashing

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,11 +1,13 @@
 extends Sprite2D
 
 var game_manager
+
+@export var starting_pos = Vector2(300, 400)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	game_manager = get_node("/root/GameManager")
+	position = starting_pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $Area2D.overlaps_area(game_manager.player.get_children()[0]):
-		game_manager.player_death()
+	pass
