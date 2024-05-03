8b7b4b24810d90c7716f3609c272e9fce6a01673
Jordan <jordansjsalazar@gmail.com>
2024-05-03 14:43:08 -0700

    something

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,20 +3,26 @@ extends Node
 var root
 var player
 var ui
+var level
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	root = get_tree().root
-	player = get_node("/root/Level/SubViewportContainer/SubViewport/Player")
-	ui = get_node("/root/Level/SubViewportContainer/CanvasLayer/UI")
+	print(root.get_children())
+	level = get_node("/root/Root/SubViewportContainer/SubViewport/Level")
+	ui = get_node("/root/Root/SubViewportContainer/CanvasLayer/UI")
+	player = level.get_children()[0]
 	ui.show()
+	level.set_process_mode(PROCESS_MODE_DISABLED)
 	debug()
 
 func player_death():
 	ui.show()
+	level.set_process_mode(PROCESS_MODE_DISABLED)
 	player.reset_pos()
 
 func debug():
-	get_node("/root/Level/SubViewportContainer/SubViewport/TileMap").queue_free()
+	ui.hide()
+	level.set_process_mode(PROCESS_MODE_INHERIT)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
